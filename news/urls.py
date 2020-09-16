71c2955eda9ba720ff8ac4fc97d9339bfe138cc9
alda27 <amunozro8970@universidadean.edu.co>
2020-09-16 02:57:41 -0500

    added new url to handle save news from user

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('<str:category>/', views.news_list, name='news_list'),
+    path('<str:category>/<int:id>/', views.news_list, name='news_list'),
     path('<str:category>/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.news_detail, name='news_detail'),
-
+    path('save/', views.news_save, name='news_save')
 ]
