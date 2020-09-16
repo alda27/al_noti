31672e0cc3354c699ed324d178dd4ad621df19c0
alda27 <amunozro8970@universidadean.edu.co>
2020-09-16 02:56:23 -0500

    added field user_save for saving news

--- news/models.py
+++ news/models.py
@@ -37,6 +37,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('news:news_list', args=[self.name, self.id])
+
 
 class News(models.Model):
     STATUS_CHOICES = (
@@ -49,14 +52,14 @@ class News(models.Model):
     slug = models.SlugField(max_length=300, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_news', verbose_name='Autor de la Noticia')
     tags = models.ManyToManyField(Tag, related_name='tag_news', verbose_name='Tags de la noticia')
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, verbose_name='Categoria de la Noticia') #
-    # TODO: add related name
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, verbose_name='Categoria de la Noticia') #  TODO: add related name
     photo = models.ImageField(upload_to='news/%Y/%m/%d/')
     is_popular = models.BooleanField(default=False)
     publish = models.DateTimeField(default=timezone.now, verbose_name='Fecha de Publicación')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de Actualización')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='borrador', verbose_name='Estado de la Noticia')
+    user_save = models.ManyToManyField(User, related_name='news_saved', blank=True)
     objects = models.Manager()  # the default manager
     published = PublishedManager()  # Our custom manager
 
