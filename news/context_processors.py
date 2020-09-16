e9d920d1facd65096074254784ea574bfd19601b
alda27 <amunozro8970@universidadean.edu.co>
2020-09-16 02:54:35 -0500

    fixed queryset to database to get categories with filter is_menu field

--- news/context_processors.py
+++ news/context_processors.py
@@ -1,6 +1,8 @@
 from .models import Category
 
 
+# context processor used to get all categories will be part of the menu base.html
+
 def category(request):
-    categories_link = Category.objects.all()
+    categories_link = Category.objects.all().filter(is_menu=True)
     return {'categories_link': categories_link}
