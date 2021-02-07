e5f24f82a0776099cde2a3c99577772b1d0c4af4
alda27 <amunozro8970@universidadean.edu.co>
2021-02-06 23:15:38 -0500

    added url for creating a new news

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,7 @@ app_name = 'news'
 urlpatterns = [
     path('<str:category>/<int:id>/', views.news_list, name='news_list'),
     path('<str:category>/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.news_detail, name='news_detail'),
+    path('create/', views.create_news, name='create_news'),
     path('save/', views.news_save, name='news_save'),
     path('search/', views.search_news, name='search_news'),
 ]
