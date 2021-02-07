131871cc8ae8a95ed529135651e1b2878564417b
alda27 <amunozro8970@universidadean.edu.co>
2021-02-06 23:19:40 -0500

    added some improvements for a structure app

--- account/urls.py
+++ account/urls.py
@@ -5,13 +5,13 @@ app_name = 'account'
 
 urlpatterns = [
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
-    path('dashboard/save-news', views.dashboard_news, name='dashboard_news'),
-    path('dashboard/articles', views.dashboard_article, name='dashboard_articles'),
+    path('dashboard/mis-noticias-guardadas', views.dashboard_news, name='dashboard_news'),
+    path('dashboard/mis-articulos', views.dashboard_article, name='dashboard_articles'),
     # path('remove/<int:id>', views.remove_news, name='remove_news'),
-    path('dashboard/profile-edit', views.edit_profile, name='edit_profile'),
-    path('login', views.login_user, name='login'),
-    path('register', views.register_user, name='register'),
-    path('logout', views.logout_user, name='logout'),
+    path('dashboard/editar-perfil', views.edit_profile, name='edit_profile'),
+    path('login/', views.login_user, name='login'),
+    path('register/', views.register_user, name='register'),
+    path('logout/', views.logout_user, name='logout'),
     # path('remove/news/<int:id_news>', views.delete_user_news, name='remove-news'),
 
 ]
